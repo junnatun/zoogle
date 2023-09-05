ff9f3dfb27ae648a72afabc670484c01a6dfd692
junnatun <junsa02@gmail.com>
2023-09-05 17:24:37 +0700

    masuk pak ikhwan

---
+++ app/src/main/java/com/junnatun/zoogleapp/ui/LinearLayout.kt
@@ -0,0 +1,12 @@
+package com.junnatun.zoogleapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.junnatun.zoogleapp.R
+
+class LinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
