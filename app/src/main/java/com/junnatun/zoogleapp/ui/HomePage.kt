afb1a8738f23c7ce827443dbd5078f8552e4957f
imanurchasanah <imanurchasanah@gmail.com>
2023-09-15 09:47:04 +0700

    add home page

---
+++ app/src/main/java/com/junnatun/zoogleapp/ui/HomePage.kt
@@ -0,0 +1,12 @@
+package com.junnatun.zoogleapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.junnatun.zoogleapp.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
